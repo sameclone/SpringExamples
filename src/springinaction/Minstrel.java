078b6d96e37f8cd4188f8dc8e85072f3672671ee
Zuyeva Yevgeniya <same_clone@mail.ru>
2017-01-08 14:29:50 +0600

    example with aop

---
+++ src/springinaction/Minstrel.java
@@ -0,0 +1,14 @@
+package springinaction;
+
+/**
+ * Created by Monk on 08.01.2017.
+ */
+public class Minstrel {
+    public void signBeforeQuest(){
+        System.out.println("Fa la la; The knight is so brave!!!!");
+    }
+
+    public  void signAfterQuest(){
+        System.out.println("Tee hee he; The brave knight did embark on a quest!");
+    }
+}
